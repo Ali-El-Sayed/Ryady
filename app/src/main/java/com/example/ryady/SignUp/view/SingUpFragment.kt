429d7a3e0f1a35086ff2785a0712d996811f17a7
Mohamed Hussein <68174156+moahamdhussein@users.noreply.github.com>
2024-05-26 01:26:22 +0300

    Update Login and signup update location Path to adapt with MVVM and delete animation

--- app/src/main/java/com/example/ryady/SignUp/view/SingUpFragment.kt
+++ app/src/main/java/com/example/ryady/SignUp/view/SingUpFragment.kt
@@ -1,10 +1,11 @@
-package com.example.ryady
+package com.example.ryady.SignUp.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.ryady.R
 
 
 class SingUpFragment : Fragment() {
