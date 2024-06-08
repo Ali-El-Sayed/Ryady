fe43f33129ecfb40f34499e04490a45825a4ad2e
Ali El-Sayed <63204358+Ali-El-Sayed@users.noreply.github.com>
2024-06-08 15:24:48 +0300

    feat(orderDetails): Impl Order Details Screens

--- app/src/main/java/com/example/ryady/view/screens/order/view/OrderDetailsFragment.kt
+++ app/src/main/java/com/example/ryady/view/screens/order/view/OrderDetailsFragment.kt
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Address
 import android.location.Geocoder
 import android.location.LocationManager
 import android.os.Build
@@ -23,12 +24,12 @@ import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ryady.R
 import com.example.ryady.databinding.FragmentOrderDetailsBinding
 import com.example.ryady.datasource.remote.RemoteDataSource
-import com.example.ryady.model.PaymentMethod
 import com.example.ryady.network.GraphqlClient
 import com.example.ryady.view.factory.ViewModelFactory
 import com.example.ryady.view.screens.order.viewmodel.OrderViewModel
@@ -38,15 +39,17 @@ import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.paymob.paymob_sdk.ui.PaymobSdkListener
 import com.skydoves.powerspinner.IconSpinnerAdapter
 import com.skydoves.powerspinner.IconSpinnerItem
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.io.IOException
 import java.util.Locale
 
-class OrderDetailsFragment : Fragment(), PaymobSdkListener {
+class OrderDetailsFragment() : Fragment() {
     private val DELAY_IN_LOCATION_REQUEST = 2000000L
     private val REQUEST_LOCATION_PERMISSION = 1
+    private var address: Address? = null
     private val TAG = "OrderDetailsFragment"
     private val binding by lazy { FragmentOrderDetailsBinding.inflate(layoutInflater) }
     private val viewModel: OrderViewModel by lazy {
@@ -63,7 +66,7 @@ class OrderDetailsFragment : Fragment(), PaymobSdkListener {
             super.onLocationResult(p0)
             val location = p0.lastLocation
             location?.let {
-                getLocationFromCoordinates(requireContext(), it.latitude, it.longitude)
+                getLocationFromCoordinates(requireActivity(), it.latitude, it.longitude)
                 hideLoadingIndicator()
             }
         }
@@ -72,26 +75,6 @@ class OrderDetailsFragment : Fragment(), PaymobSdkListener {
         LocationServices.getFusedLocationProviderClient(requireActivity())
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding.tvGetCurrentLocation.setOnClickListener {
-            if (isGPSEnabled(requireContext())) getFreshLocation(locationCallback)
-            else showEnableGPSDialog()
-        }
-        binding.btnCheckout.setOnClickListener {
-            // Save Order Shipping Address
-            viewModel.currentOrder.shippingAddress = binding.etCustomerLocation.text.toString()
-            // Save Order Payment Method
-            viewModel.currentOrder.paymentMethod.let {
-                val selectedItem = binding.spinner.selectedIndex
-                if (selectedItem == 0) {
-                    viewModel.currentOrder.paymentMethod = PaymentMethod.CASH_ON_DELIVERY
-                } else if (selectedItem == 1) {
-                    viewModel.currentOrder.paymentMethod = PaymentMethod.CREDIT_CARD
-                }
-            }
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -99,6 +82,11 @@ class OrderDetailsFragment : Fragment(), PaymobSdkListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // Get Current Location
+        binding.tvGetCurrentLocation.setOnClickListener {
+            if (isGPSEnabled(requireActivity())) getFreshLocation(locationCallback)
+            else showEnableGPSDialog()
+        }
         // Payment Method Spinner
         binding.spinner.apply {
             setSpinnerAdapter(IconSpinnerAdapter(this))
@@ -109,23 +97,31 @@ class OrderDetailsFragment : Fragment(), PaymobSdkListener {
                     ), IconSpinnerItem(text = context.getString(R.string.credit_card), iconRes = R.drawable.ic_credit_card)
                 )
             )
-            getSpinnerRecyclerView().layoutManager = LinearLayoutManager(requireContext())
+            getSpinnerRecyclerView().layoutManager = LinearLayoutManager(requireActivity())
             selectItemByIndex(1) // select a default item.
             lifecycleOwner = lifecycleOwner
         }
         // Back Button
         binding.btnCancel.setOnClickListener { findNavController().popBackStack() }
-
         // Checkout Button
         binding.btnCheckout.setOnClickListener {
-
+            viewModel.currentOrder.shippingAddress = binding.etCustomerLocation.text.toString()
+            lifecycleScope.launch(Dispatchers.IO) {
+                viewModel.currentOrder.countryCode = address?.countryCode ?: ""
+                viewModel.currentOrder.city = address?.adminArea ?: ""
+                viewModel.currentOrder.postalCode = address?.postalCode ?: ""
+                viewModel.currentOrder.countryName = address?.countryName ?: ""
+                Log.d(TAG, "onViewCreated: $address")
+                Log.d(TAG, "onViewCreated: ${viewModel.currentOrder}")
+                viewModel.createOrderInformation()
+            }
         }
     }
 
     @SuppressLint("MissingPermission")
     private fun getFreshLocation(callback: LocationCallback) {
-        if (isGPSPermissionGranted(requireContext())) {
-            if (isGPSEnabled(requireContext())) {
+        if (isGPSPermissionGranted(requireActivity())) {
+            if (isGPSEnabled(requireActivity())) {
                 showLoadingIndicator()
                 fusedLocationProviderClient.requestLocationUpdates(
                     LocationRequest.Builder(DELAY_IN_LOCATION_REQUEST).apply {
@@ -135,9 +131,7 @@ class OrderDetailsFragment : Fragment(), PaymobSdkListener {
                     Log.d(TAG, "Error getting location: ${it.message}")
                 }
             } else showEnableGPSDialog()
-        } else {
-            requestPermission()
-        }
+        } else requestPermission()
     }
 
 
@@ -148,12 +142,12 @@ class OrderDetailsFragment : Fragment(), PaymobSdkListener {
             geocoder.getFromLocation(latitude, longitude, 1) {
                 try {
                     if (it.size > 0) {
-                        val address = it[0]
-                        val locationName = address.getAddressLine(0)
+                        address = it[0]
+                        val locationName = address?.getAddressLine(0) ?: ""
                         binding.etCustomerLocation.text = Factory.getInstance().newEditable(locationName)
                     }
                 } catch (e: Exception) {
-                    Toast.makeText(requireContext(), "Try Again", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(requireActivity(), "Try Again", Toast.LENGTH_SHORT).show()
                 }
             }
         } catch (e: IOException) {
@@ -227,16 +221,8 @@ class OrderDetailsFragment : Fragment(), PaymobSdkListener {
         binding.frameLayout.visibility = View.GONE
     }
 
-
-    override fun onFailure() {
-        TODO("Not yet implemented")
-    }
-
-    override fun onPending() {
-        TODO("Not yet implemented")
-    }
-
-    override fun onSuccess() {
-        TODO("Not yet implemented")
+    override fun onDestroy() {
+        super.onDestroy()
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
     }
 }
--- app/src/main/java/com/example/ryady/view/screens/order/viewmodel/OrderViewModel.kt
+++ app/src/main/java/com/example/ryady/view/screens/order/viewmodel/OrderViewModel.kt
@@ -1,10 +1,21 @@
 package com.example.ryady.view.screens.order.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ryady.datasource.remote.IRemoteDataSource
 import com.example.ryady.model.Order
+import kotlinx.coroutines.launch
 
 class OrderViewModel(private val remote: IRemoteDataSource) : ViewModel() {
     var currentOrder: Order = Order()
 
+
+    suspend fun createOrderInformation() {
+        viewModelScope.launch {
+            remote.createOrderInformation(
+                "f4093054bf8cf9c70e84961dd8a27ed3",
+                currentOrder
+            )
+        }
+    }
 }
